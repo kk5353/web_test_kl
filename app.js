9abf2cc46c336b30786e1fcc937a38da555d6cab
kk5353 <kk5353@aliyun.com>
2019-04-22 15:53:42 +0800

    新增画图

--- app.js
+++ app.js
@@ -1,12 +1,8 @@
 const express = require('express');
 const history = require('connect-history-api-fallback');
 var server = express();
-server.listen(8080);
-console.log('http://127.0.0.1:8080'  );
-server.use(history({
-    verbose: true,
-    index: '/'
-}));
+server.listen(808);
+console.log('http://127.0.0.1:808'  );
 server.use('/',express.static('static'));
 
 
