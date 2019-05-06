c0c7900ca5fcce038fc8948def9571d21d04fff0
lei kong <kk5353@aliyun.com>
2019-05-06 12:24:16 +0800

    add ws

--- app.js
+++ app.js
@@ -2,8 +2,5 @@ const express = require('express');
 const history = require('connect-history-api-fallback');
 var server = express();
 server.listen(808);
-console.log('http://127.0.0.1:808'  );
-server.use('/',express.static('static'));
-
-
-
+console.log('http://127.0.0.1:808');
+server.use('/', express.static('static'));
